073fd59c114ee2914010ca9409faeef27bb8d2b2
Makhmudbek <rustamov.mahmud.05@gmail.com>
2025-07-09 16:06:23 +0500

    I imported ozodbek's code and add mine

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("Hello, World!!")
+import json
+import os
+
+FILENAME = "registrations.json"
+LIMIT = 5
+
+default_slots = {
+    "12:00 - 12:30": [],
+    "12:30 - 13:00": [],
+    "13:00 - 13:30": []
+}
+
+def load_slots():
+    if os.path.exists(FILENAME):
+        with open(FILENAME, "r") as file:
+            return json.load(file)
+    return default_slots.copy()
